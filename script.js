021b5a46e15fa3bf1dd20c71f5a97246c2ceef56
CRosal04 <156229740+CRosal04@users.noreply.github.com>
2025-04-08 15:47:31 -0700

    fixed

---
+++ script.js
@@ -0,0 +1,29 @@
+let cupcakesCollection = null;
+
+$.get("https://6f186305-ab26-4839-b806-380e3560e049.mock.pstmn.io/cupcakes.json", function(data){
+    displayCupcakes(data);
+
+    cupcakesCollection = data;
+
+}   ,"json").fail(function(){
+$("#cupcakes").empty();
+$("#cupcakes").append("<div>there was a problem trying to connect with the server, please try again</div>");
+});
+
+function displayCupcakes(data){
+    $("#cupcakes").empty();
+    let html = "";
+    ///iterate through cupcakes
+    for (let cupcake of data){
+        html += 
+        `<section>
+        <img src ="${cupcake.image}" alt = "${cupcakes.alt}">
+        <h4>${cupcake.name}</h4>
+        <b>Ingredients:</b>
+        <p class="ingredients">${cupcake.ingredients}</p>
+        <b>${cupcake.frosting} Frosting</b>
+        <p class="frosting">${cupcake.frostingIngredients}</p>
+        </section>`;
+    }
+    $("#cupcakes").html(html);
+}
